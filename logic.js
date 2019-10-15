3272f9e80efed7a5ba72a4edae362052b0058a39
Ken F <kenf@tuta.io>
2019-10-15 15:50:27 -0600

    Add content

---
+++ logic.js
@@ -0,0 +1,40 @@
+$(window).on('hashchange', function() {
+  ChangeTheme();
+});
+
+$(document).ready(function() {
+  ChangeTheme();
+
+  $('.wrapper').on('mouseover', function() {
+    $(this).addClass('hover')
+  }).on('mouseleave', function() {
+    $(this).removeClass('hover')
+  });
+
+})
+
+function ChangeTheme() {
+  Data = location.hash.replace( /^#/, '' );
+  Search = Data.search('=');
+  if(Search !== -1) {
+    DataSplit = Data.split('=');
+    if(DataSplit[0] === 'theme') {
+      if(DataSplit[1] === 'light' || DataSplit[1] === 'lightdark' || DataSplit[1] === 'darklight' || DataSplit[1] === 'dark') {
+        ChangeThemeArg(DataSplit[1]);
+      } else {
+        ChangeThemeArg('light');
+      }
+    } else {
+      ChangeThemeArg('light');
+    }
+  } else {
+    ChangeThemeArg('light');
+  }
+}
+
+function ChangeThemeArg(arg) {
+  $('body').removeClass();
+  $('body').addClass(arg);
+  $('.themes .wrapper').removeClass('active');
+  $('#' + arg).parent().addClass('active');
+}
